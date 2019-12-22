3f9bd09ddddcdc0ab883bc72516de35d062701eb
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-12-22 23:02:50 +0800

    create license key for clients

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -18,9 +18,10 @@ class PermissionController extends Controller
      */
     public function index()
     {
-        return view('Pages.Permissions.index')->with([
-            'roles' => \App\Role::all()
-        ]);
+        return view('pages.Permissions.index');
+//        return view('Pages.Permissions.index')->with([
+//            'roles' => \App\Role::all()
+//        ]);
     }
 
     /**
