392e999237838b22d6fdb04a4d9dfde171c47844
kevinpauneljohn <johnkevinpaunel@gmail.com>
2019-12-26 22:43:03 +0800

    create license key for clients

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    /**
+     * Dec. 26, 2019
+     * @author john kevin paunel
+     * authenticated the username and password submitted
+     * route: login.authenticate
+     * @param Request $request
+     * @return mixed
+     * */
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'username'      => 'required',
+            'password'      => 'required',
+        ],[
+            'username.required' => 'Username is required',
+            'password.required' => 'Password is required',
+        ]);
+
+        $credential = $request->only('username','password');
+
+        if(Auth::attempt($credential))
+        {
+            activity()->causedBy(auth()->user()->id)->withProperties(['username' => auth()->user()->username])->log('user logged in');
+            $accessToken = auth()->user()->createToken('authToken')->accessToken;
+            //activity()->causedBy(auth()->user()->id)->withProperties(['username' => auth()->user()->username])->log('user logged in');
+            return response()->json([
+                'user' => auth()->user(),
+                'access_token' => $accessToken,
+                'success'   => true,
+                ]);
+        }
+        return response()->json(['message' => 'invalid credentials', 'success' => false]);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,9 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Gate::before(function ($user, $ability) {
-            return $user->hasRole('Super Admin') ? true : null;
-        });
+//        Gate::before(function ($user, $ability) {
+//            return $user->hasRole('Super Admin') ? true : null;
+//        });
 
         Passport::routes();
     }
