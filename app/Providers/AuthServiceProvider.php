3bbe0e722c0a0aa123061472ef707f42635584fa
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-02 23:26:42 +0800

    create license key for clients

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,10 +26,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-//        Gate::before(function ($user, $ability) {
-//            return $user->hasRole('Super Admin') ? true : null;
-//        });
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
+        });
 
         Passport::routes();
+//        Passport::tokensCan([
+//            'create-client' => 'Create new Client'
+//        ]);
     }
 }
