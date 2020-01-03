3824fb79407ae582b870d571297e0e9feaccbdb3
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-03 20:24:47 +0800

    create license key for clients

--- app/Terminal.php
+++ app/Terminal.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
+use App\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Terminal extends Model
 {
-    //
+    use SoftDeletes, LogsActivity, UsesUuid;
+
+    protected $fillable = ['user_id','device'];
+    protected static $logAttributes = ['user_id','device'];
+
+    /**
+     * Jan. 03, 2020
+     * @author john kevin paunel
+     * retrieve user terminal details
+     * */
+    public function users()
+    {
+
+    }
 }
