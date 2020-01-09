e981f505f183f04cf8164799928ad1f7bb68a740
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-09 09:41:10 +0800

    create license key for clients

--- app/Http/Controllers/Api/ThresholdController.php
+++ app/Http/Controllers/Api/ThresholdController.php
@@ -9,23 +9,40 @@ use Illuminate\Support\Facades\DB;
 
 class ThresholdController extends Controller
 {
+    /**
+     * Jan. 09, 2020
+     * @author john kevin paunel
+     * save the threshold data from local to server
+     * @param Request $request
+     * @return mixed
+     * */
     public function save(Request $request)
     {
+        /*retrieve all receiver terminals*/
         $terminals = Terminal::where([
             ['user_id','=',$request->causer_id],
-            ['terminal_id','!=',$request->terminal_id],
+            ['id','!=',$request->terminal_id],
         ])->get();
+
 
         foreach($terminals as $terminal)
         {
-
+            /*insert request data to each receiver terminal*/
+            $threshold = DB::table('thresholds')->insert([
+                'causer_id'     => $request->causer_id,
+                'terminal_id'     => $request->terminal_id,
+                'data'     => $request->data,
+                'action'     => $request->action,
+                'receiver_terminal'     => $terminal->id,
+                'created_at'     => $request->created_at,
+                'updated_at'     => $request->updated_at,
+            ]);
         }
-        $threshold = DB::table('thresholds')->insert($request->all());
         if($threshold)
         {
             return 1;
         }else{
-            return 2;
+            return 0;
         }
     }
 }
