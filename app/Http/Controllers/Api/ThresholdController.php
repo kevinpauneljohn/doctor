9b3ff2b488afd4f2112c77fe50241f92cf9db599
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-10 08:15:58 +0800

    create license key for clients

--- app/Http/Controllers/Api/ThresholdController.php
+++ app/Http/Controllers/Api/ThresholdController.php
@@ -21,7 +21,8 @@ class ThresholdController extends Controller
      * */
     public function save(Request $request)
     {
-        return "working";
+        //return "working";
+        return $request->all();
     }
 //    public function save(Request $request)
 //    {
