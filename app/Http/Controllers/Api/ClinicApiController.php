a568944f3d8bff10308fe8b1a0294beff6227c17
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-15 21:08:27 +0800

    create license key for clients

--- app/Http/Controllers/Api/ClinicApiController.php
+++ app/Http/Controllers/Api/ClinicApiController.php
@@ -10,7 +10,17 @@ class ClinicApiController extends Controller
 {
     public function store(Request $request)
     {
-        return $request->all();
-        //$clinic = DB::table('clinics')->insert($request->all());
+        //return $request->all();
+//        $clinic = DB::table('clinics')->insert([
+//            'id'    => $request->id,
+//            'name'    => $request->name,
+//            'address'    => $request->address,
+//            'landline'    => $request->landline,
+//            'mobile'    => $request->mobile,
+//            'status'    => $request->status,
+//            'created_at'    => $request->created_at,
+//            'updated_at'    => $request->updated_at,
+//        ]);
+        $clinic = DB::table('clinics')->insert($request->all());
     }
 }
