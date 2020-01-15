9d044fcb444cc1ebc3366ca589547b2c2dbc540b
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-15 20:51:52 +0800

    create license key for clients

--- app/Http/Controllers/Api/ClinicApiController.php
+++ app/Http/Controllers/Api/ClinicApiController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ClinicApiController extends Controller
 {
     public function store(Request $request)
     {
-        return $request->all();
+        //return $request->all();
+        $clinic = DB::table('clinics')->insert($request->all());
     }
 }
