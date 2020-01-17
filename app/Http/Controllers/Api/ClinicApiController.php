3c98c0151da8f08f1f70da77409c38c06d01e2f6
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-17 12:11:03 +0800

    create license key for clients

--- app/Http/Controllers/Api/ClinicApiController.php
+++ app/Http/Controllers/Api/ClinicApiController.php
@@ -20,38 +20,15 @@ class ClinicApiController extends Controller
      * */
     public function store(Request $request)
     {
-        //$terminals = Terminal::where('user_id',$request->user_id)->get();
-
-        $terminals = Terminal::where([
-            ['user_id','=',$request->user_id],
-            ['id','!=',$request->terminal_id],
-        ]);
-
-        if($terminals->count() > 0)
-        {
-            foreach ($terminals->get() as $terminal)
-            {
-                $threshold = DB::table('thresholds')->insert([
-                    'causer_id'     => $request->user_id,
-                    'terminal_id'     => $request->terminal_id,
-                    'data'     => json_encode($request->all()),
-                    'action'     => $request->action,
-                    'receiver_terminal'     => $terminal->id,
-                    'created_at'     => $request->created_at,
-                    'updated_at'     => $request->updated_at,
-                ]);
-            }
-            if($threshold)
-            {
-                return "1";
-            }
-        }
-
-        return "0";
-        //$this->threshold($request)->save($request);
-//        return $request->all();
+        $this->threshold($request)->save($request);
+        return $request->all();
     }
 
+    /**
+     * Jan. 17, 2020
+     * @author john kevin paunel
+     * save the clinic data to clinics table
+     * */
     public function save($request)
     {
         DB::table('clinics')->insert([
@@ -71,29 +48,36 @@ class ClinicApiController extends Controller
         return $this;
     }
 
+    /**
+     * Jan. 17, 2020
+     * @author john kevin paunel
+     * save the data to threshold for all the receiver terminals
+     * */
     public function threshold($request)
     {
         /*retrieve all receiver terminals*/
         $terminals = Terminal::where([
-            ['user_id','=',$request->causer_id],
+            ['user_id','=',$request->user_id],
             ['id','!=',$request->terminal_id],
-        ])->get();
+        ]);
 
-        $terminals = Terminal::all();
-
-        foreach ($terminals as $terminal)
+        /*check if there is available terminal who will receive*/
+        if($terminals->count() > 0)
         {
-            DB::table('thresholds')->insert([
-                'causer_id'     => $request->user_id,
-                'terminal_id'     => $request->terminal_id,
-                'data'     => json_encode($request->all()),
-                'action'     => $request->action,
-                'receiver_terminal'     => $terminal->id,
-                'created_at'     => $request->created_at,
-                'updated_at'     => $request->updated_at,
-            ]);
+            foreach ($terminals->get() as $terminal)
+            {
+                /*save the data to threshold*/
+                $threshold = DB::table('thresholds')->insert([
+                    'causer_id'     => $request->user_id,
+                    'terminal_id'     => $request->terminal_id,
+                    'data'     => json_encode($request->all()),
+                    'action'     => $request->action,
+                    'receiver_terminal'     => $terminal->id,
+                    'created_at'     => $request->created_at,
+                    'updated_at'     => $request->updated_at,
+                ]);
+            }
         }
-
 
         return $this;
     }
