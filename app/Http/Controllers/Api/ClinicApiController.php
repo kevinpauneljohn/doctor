114147906c1f5976a7827812655ad3f27d33e257
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-17 23:10:15 +0800

    create license key for clients

--- app/Http/Controllers/Api/ClinicApiController.php
+++ app/Http/Controllers/Api/ClinicApiController.php
@@ -88,7 +88,11 @@ class ClinicApiController extends Controller
 
     public function updateClinic(Request $request)
     {
-        return response()->json(['data' => $request->id]);
+        if($this->update($request)->threshold($request))
+        {
+            return 1;
+        }
+        return 0;
     }
 
     public function update($request)
