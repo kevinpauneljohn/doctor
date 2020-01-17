e7985ef4083e93861a6d33675b1df0cfddb5dbd2
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-17 09:48:07 +0800

    create license key for clients

--- app/Http/Controllers/Api/ClinicApiController.php
+++ app/Http/Controllers/Api/ClinicApiController.php
@@ -3,25 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Terminal;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ClinicApiController extends Controller
 {
+
+    /**
+     * Jan. 17, 2019
+     * @author john kevin paunel
+     * save to clinics table
+     * @param Request $request
+     * @return mixed
+     * */
     public function store(Request $request)
     {
-        //return $request->all();
 //        $clinic = DB::table('clinics')->insert([
-//            'id'    => $request->id,
-//            'name'    => $request->name,
-//            'address'    => $request->address,
-//            'landline'    => $request->landline,
-//            'mobile'    => $request->mobile,
-//            'status'    => $request->status,
+//            'id'            => $request->id,
+//            'name'          => $request->name,
+//            'address'       => $request->address,
+//            'state'         => $request->state,
+//            'city'          => $request->city,
+//            'landline'      => $request->landline,
+//            'mobile'        => $request->mobile,
+//            'user_id'       => $request->user_id,
+//            'status'        => $request->status,
 //            'created_at'    => $request->created_at,
 //            'updated_at'    => $request->updated_at,
+//            'terminal_id'   => $request->terminal_id,
 //        ]);
-        $clinic = DB::table('clinics')->insert($request->all());
         return $request->all();
+    }
+
+    public function threshold(Request $request)
+    {
+        /*retrieve all receiver terminals*/
+        $terminals = Terminal::where([
+            ['user_id','=',$request->causer_id],
+            ['id','!=',$request->terminal_id],
+        ])->get();
     }
 }
