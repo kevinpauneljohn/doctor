ed12ea9ccedd064af05785339f89e16d8cbad6fe
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-17 12:06:09 +0800

    create license key for clients

--- app/Http/Controllers/Api/ClinicApiController.php
+++ app/Http/Controllers/Api/ClinicApiController.php
@@ -25,24 +25,28 @@ class ClinicApiController extends Controller
         $terminals = Terminal::where([
             ['user_id','=',$request->user_id],
             ['id','!=',$request->terminal_id],
-        ])->get();
+        ]);
 
-        foreach ($terminals as $terminal)
+        if($terminals->count() > 0)
         {
-            $threshold = DB::table('thresholds')->insert([
-                'causer_id'     => $request->user_id,
-                'terminal_id'     => $request->terminal_id,
-                'data'     => json_encode($request->all()),
-                'action'     => $request->action,
-                'receiver_terminal'     => $terminal->id,
-                'created_at'     => $request->created_at,
-                'updated_at'     => $request->updated_at,
-            ]);
+            foreach ($terminals->get() as $terminal)
+            {
+                $threshold = DB::table('thresholds')->insert([
+                    'causer_id'     => $request->user_id,
+                    'terminal_id'     => $request->terminal_id,
+                    'data'     => json_encode($request->all()),
+                    'action'     => $request->action,
+                    'receiver_terminal'     => $terminal->id,
+                    'created_at'     => $request->created_at,
+                    'updated_at'     => $request->updated_at,
+                ]);
+            }
+            if($threshold)
+            {
+                return "1";
+            }
         }
-        if($threshold)
-        {
-            return "1";
-        }
+
         return "0";
         //$this->threshold($request)->save($request);
 //        return $request->all();
