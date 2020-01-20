95ae99ffe3532bd9d0d3f97fcecfc59c2a85bbfc
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-20 23:46:55 +0800

    create license key for clients

--- app/Http/Controllers/Api/MedicalStaffApiController.php
+++ app/Http/Controllers/Api/MedicalStaffApiController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MedicalStaffApiController extends Controller
 {
     public function createMedicalStaff(Request $request)
     {
-        return $request->all();
+        return $request->firstname;
 
+    }
+
+    public function staff($request)
+    {
+//        DB::table('users')->insert([
+//            'id'
+//        ]);
     }
 }
