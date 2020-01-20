37e536cd9b22fa7d687bff21454d3377837bcb68
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-21 00:41:01 +0800

    create license key for clients

--- app/Http/Controllers/Api/MedicalStaffApiController.php
+++ app/Http/Controllers/Api/MedicalStaffApiController.php
@@ -23,32 +23,36 @@ class MedicalStaffApiController extends Controller
      * */
     public function staff($request)
     {
-//        $staff = DB::table('users')->insert([
-//            'id'    => $request->user_id,
-//            'firstname'    => $request->firstname,
-//            'middlename'    => $request->middlename,
-//            'lastname'    => $request->lastname,
-//            'mobileNo'    => $request->mobileNo,
-//            'address'    => $request->address,
-//            'refprovince'    => $request->refprovince,
-//            'refcitymun'    => $request->refcitymun,
-//            'status'    => $request->status,
-//            'category'    => $request->category,
-//            'created_at'    => $request->created_at,
-//            'updated_at'    => $request->updated_at,
-//        ]);
-        $staff = new User();
-        $staff->id = $request->user_id;
-        $staff->firstname = $request->firstname;
-        $staff->middlename = $request->middlename;
-        $staff->lastname = $request->lastname;
-        $staff->mobileNo = $request->mobileNo;
-        $staff->address = $request->address;
-        $staff->status = $request->status;
-        $staff->category = $request->category;
-        $staff->created_at = $request->created_at;
-        $staff->updated_at = $request->updated_at;
-        $staff->save();
+        $staff = DB::table('users')->insert([
+            'id'    => $request->user_id,
+            'firstname'    => $request->firstname,
+            'middlename'    => $request->middlename,
+            'lastname'    => $request->lastname,
+            'mobileNo'    => $request->mobileNo,
+            'address'    => $request->address,
+            'refprovince'    => $request->refprovince,
+            'refcitymun'    => $request->refcitymun,
+            'status'    => $request->status,
+            'category'    => $request->category,
+            'created_at'    => $request->created_at,
+            'updated_at'    => $request->updated_at,
+        ]);
+
+        return User::find($request->user_id);
+//        $staff = new User();
+//        $staff->id = $request->user_id;
+//        $staff->firstname = $request->firstname;
+//        $staff->middlename = $request->middlename;
+//        $staff->lastname = $request->lastname;
+//        $staff->mobileNo = $request->mobileNo;
+//        $staff->address = $request->address;
+//        $staff->refprovince = $request->refprovince;
+//        $staff->refcitymun = $request->refcitymun;
+//        $staff->status = $request->status;
+//        $staff->category = $request->category;
+//        $staff->created_at = $request->created_at;
+//        $staff->updated_at = $request->updated_at;
+//        $staff->save();
         return $this;
     }
 
