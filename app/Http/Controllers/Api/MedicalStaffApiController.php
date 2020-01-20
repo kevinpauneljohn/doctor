75975fcf31cbb134a9d1ed09552eac869a3f31ec
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-20 22:59:40 +0800

    create license key for clients

--- app/Http/Controllers/Api/MedicalStaffApiController.php
+++ app/Http/Controllers/Api/MedicalStaffApiController.php
@@ -13,7 +13,7 @@ class MedicalStaffApiController extends Controller
 
         foreach ($medicalStaff as $staff)
         {
-            return $staff;
+            return $staff['firstname'];
         }
     }
 }
