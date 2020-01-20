e0c588f807b20e2673fb5a9fec7473e8ca1b6088
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-20 23:52:09 +0800

    create license key for clients

--- app/Http/Controllers/Api/MedicalStaffApiController.php
+++ app/Http/Controllers/Api/MedicalStaffApiController.php
@@ -10,14 +10,26 @@ class MedicalStaffApiController extends Controller
 {
     public function createMedicalStaff(Request $request)
     {
-        return $request->firstname;
-
+        $this->staff($request);
+        return 1;
     }
 
     public function staff($request)
     {
-//        DB::table('users')->insert([
-//            'id'
-//        ]);
+        DB::table('users')->insert([
+            'id'    => $request->id,
+            'firstname'    => $request->firstname,
+            'middlename'    => $request->middlename,
+            'lastname'    => $request->lastname,
+            'mobileNo'    => $request->mobileNo,
+            'address'    => $request->address,
+            'refprovince'    => $request->refprovince,
+            'refcitymun'    => $request->refcitymun,
+            'status'    => $request->status,
+            'created_at'    => $request->created_at,
+            'updated_at'    => $request->updated_at,
+        ]);
+
+        return $this;
     }
 }
