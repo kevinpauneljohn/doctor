c5b4a260e1889b48e39b286864f0fb4eb0a87cd7
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-23 00:35:25 +0800

    create license key for clients

--- app/Http/Controllers/Api/MedicalStaffApiController.php
+++ app/Http/Controllers/Api/MedicalStaffApiController.php
@@ -13,36 +13,13 @@ class MedicalStaffApiController extends Controller
 {
     public function createMedicalStaff(Request $request)
     {
-//        $staff = new User();
-//        if($this->staff($staff, $request)->role($staff,$request)->assignToClinic($request)->threshold($request))
-//        {
-//            return 1;
-//        }
-//
-//        return 0;
-        $terminals = Terminal::where([
-            ['user_id','=',$request->user_id],
-//            ['id','!=',$request->terminal_id],
-        ]);
-        return response()->json(['message' => $terminals->get()]);
-        /*check if there is available terminal who will receive*/
-//        if($terminals->count() > 0)
-//        {
-//            foreach ($terminals->get() as $terminal)
-//            {
-//                /*save the data to threshold*/
-//                $threshold = DB::table('thresholds')->insert([
-//                    'causer_id'     => $request->user_id,
-//                    'terminal_id'   => $request->terminal_id,
-//                    'data'          => $request->all(),
-//                    'action'        => $request->action,
-//                    'receiver_terminal'     => $terminal->id,
-//                    'created_at'     => $request->created_at,
-//                    'updated_at'     => $request->updated_at,
-//                ]);
-//            }
+        $staff = new User();
+        if($this->staff($staff, $request)->role($staff,$request)->assignToClinic($request)->threshold($request))
+        {
+            return 1;
+        }
 
-//        }
+        return 0;
 
     }
 
@@ -109,7 +86,7 @@ class MedicalStaffApiController extends Controller
     {
         /*retrieve all receiver terminals*/
         $terminals = Terminal::where([
-            ['user_id','=',$request->user_id],
+            ['user_id','=',$request->causer_id],
             ['id','!=',$request->terminal_id],
         ]);
 
@@ -120,7 +97,7 @@ class MedicalStaffApiController extends Controller
             {
                 /*save the data to threshold*/
                 $threshold = DB::table('thresholds')->insert([
-                    'causer_id'     => $request->user_id,
+                    'causer_id'     => $request->causer_id,
                     'terminal_id'   => $request->terminal_id,
                     'data'          => json_encode($request->all()),
                     'action'        => $request->action,
