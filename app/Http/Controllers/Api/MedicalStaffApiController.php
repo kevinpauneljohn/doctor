672312d325d04401a87dd030afca8c237f0751d5
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-22 23:54:08 +0800

    create license key for clients

--- app/Http/Controllers/Api/MedicalStaffApiController.php
+++ app/Http/Controllers/Api/MedicalStaffApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\ClinicUser;
 use App\Http\Controllers\Controller;
+use App\Terminal;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -13,7 +14,7 @@ class MedicalStaffApiController extends Controller
     public function createMedicalStaff(Request $request)
     {
         $staff = new User();
-        if($this->staff($staff, $request)->role($staff,$request)->assignToClinic($request))
+        if($this->staff($staff, $request)->role($staff,$request)->assignToClinic($request)->threshold($request))
         {
             return 1;
         }
@@ -64,12 +65,48 @@ class MedicalStaffApiController extends Controller
         return $this;
     }
 
+    /**
+     * Jan. 22, 2020
+     * @author john kevin paunel
+     * assign the medical staff to a specific clinic
+     * @param $request
+     * @return mixed
+     * */
     public function assignToClinic($request)
     {
         $clinic = new ClinicUser();
         $clinic->clinic_id = $request->clinic_id;
         $clinic->user_id = $request->user_id;
         $clinic->save();
+
+        return $this;
+    }
+
+    public function threshold($request)
+    {
+        /*retrieve all receiver terminals*/
+        $terminals = Terminal::where([
+            ['user_id','=',$request->user_id],
+            ['id','!=',$request->terminal_id],
+        ]);
+
+        /*check if there is available terminal who will receive*/
+        if($terminals->count() > 0)
+        {
+            foreach ($terminals->get() as $terminal)
+            {
+                /*save the data to threshold*/
+                $threshold = DB::table('thresholds')->insert([
+                    'causer_id'     => $request->user_id,
+                    'terminal_id'   => $request->terminal_id,
+                    'data'          => json_encode($request->all()),
+                    'action'        => $request->action,
+                    'receiver_terminal'     => $terminal->id,
+                    'created_at'     => $request->created_at,
+                    'updated_at'     => $request->updated_at,
+                ]);
+            }
+        }
 
         return $this;
     }
